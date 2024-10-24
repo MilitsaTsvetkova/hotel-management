22ab8cebc9f189f56cdd52753d19a044c1b1602e
MilitsaTsvetkova <militsa88@yahoo.com>
2024-10-24 10:35:33 +0300

    fix build error

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -5,7 +5,7 @@ import ThemeContext from "../../context/themeContext";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const themeFromStorage =
-    typeof localStorage !== undefined && localStorage.getItem("hotel-theme")
+    typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
       ? JSON.parse(localStorage.getItem("hotel-theme")!)
       : false;
   const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
