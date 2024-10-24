f5cd5aa616332470d8f959708658aaf02bcfcd7b
MilitsaTsvetkova <militsa88@yahoo.com>
2024-10-24 10:07:12 +0300

    added Footer Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const poppins = Poppins({
-  subsets:[ "latin"],
-  weight: [ '400', '500', '700',  '900'],
-  style:['normal','italic'],
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["normal", "italic"],
   variable: "--font-poppins",
-})
-
+});
 
 export const metadata: Metadata = {
   title: "Hotel Management App",
@@ -23,12 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={poppins.className}
-      >
+      <body className={poppins.className}>
         <main className="font-normal">
           <Header />
-        {children}
+          {children}
+          <Footer />
         </main>
       </body>
     </html>
