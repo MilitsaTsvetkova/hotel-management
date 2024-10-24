c173a17ad0a9e68aeda4cb5d798e325c41fc0bb2
MilitsaTsvetkova <militsa88@yahoo.com>
2024-10-24 14:14:41 +0300

    integrated sanity.io

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import ThemeProvider from "../components/ThemeProvider/ThemeProvider";
+import ThemeProvider from "../../components/ThemeProvider/ThemeProvider";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
